8ee679b579a80097e6a94b310b3e179adf51bdea
neilsona8 <as_neilson@yahoo.ca>
2016-11-08 17:05:13 -0700

    Added a ListView Scollbar to GUI

--- NobelPrize/NobelPrize/src/nobelprize/NobelPrize.java
+++ NobelPrize/NobelPrize/src/nobelprize/NobelPrize.java
@@ -6,24 +6,32 @@
 package nobelprize;
 
 
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.collections.*;
+import javafx.beans.value.*;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import javafx.application.Application;
-import javafx.scene.*;
 import javafx.stage.Stage;
 import org.json.JSONObject;
 import java.net.MalformedURLException;
 import java.net.URLConnection;
-import javafx.event.*;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import static javafx.geometry.Pos.CENTER;
-import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import static javax.swing.text.StyleConstants.Orientation;
+
 
 /**
  *
@@ -31,16 +39,60 @@ import static javax.swing.text.StyleConstants.Orientation;
  */
 public class NobelPrize extends Application {
     
+    //@FXML
+    private Button button1; 
+    Label response;
+    Label result;
+    Label countryList;
+    Label countryPick;
+    
     //creates the user input screen
     @Override
     public void start(Stage stage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("GUIScreen.fxml"));
-        FlowPane rootPlane = new FlowPane(300,100);
-        ScrollPane results = new ScrollPane();
-        Scene scene = new Scene(rootPlane);
-        Scene resultsScene = new Scene(results);
-               
+        
+        //set the Stage title
+        stage.setTitle("Nobel Prize Quest");
+
+        //Create a Flowpane for the root node 
+        FlowPane rootPlane = new FlowPane(10,10);
+        //put elements in topleft  of rootPlane
+        rootPlane.setAlignment(Pos.TOP_LEFT);
+        //Set rootPlane inside the Scene (size of 300 X 100)
+        Scene scene = new Scene(rootPlane, 300, 100);
+
+        //Labels for the search screen
+        response = new Label("Search");
         result = new Label("");
+        countryList= new Label("Select Country");
+        countryPick=new Label ("");
+        
+        //Create a List of countries 
+        ObservableList<String> countries= FXCollections.observableArrayList(
+        "Canada","USA", "Argentina","Costa Rica","Colombia","Cyprus","Denmark");
+        
+        //Create the list view and set height and width
+        ListView<String> listViewCountries = new ListView<String> (countries);
+        listViewCountries.setPrefSize (100,50);
+        //set the selection model
+        MultipleSelectionModel<String> selectListViewCountries =listViewCountries.getSelectionModel();
+        
+        //when user selects a country, respond to change of selection
+        selectListViewCountries.selectedItemProperty().addListener(new ChangeListener<String>(){
+            public void selectionMade  (ObservableValue<? extends String> selectionMade, 
+                String defaultVal, String countryPicked){
+                //show the country picked
+                countryPick.setText("Country selected is: " + countryPicked);
+                }   
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+            });
+        
+        //create a new ScrollPane to hold the results
+        ScrollPane results = new ScrollPane();
+        Scene resultsScene = new Scene(results);
         results.setContent(result);
         
         stage.setScene(scene);
@@ -56,16 +108,14 @@ public class NobelPrize extends Application {
             }
         });
         
-        response = new Label("Push button");
-        
-        rootPlane.getChildren().addAll(button1, response);
+        //rootPlane.getChildren().addAll(button1, response);
+        rootPlane.getChildren().addAll(listViewCountries, countryPick);
         stage.show();
+
+       
     }
     
-    //@FXML
-    private Button button1; 
-    Label response;
-    Label result;
+    
     
     //This function creates a JSON object in order to read the API URL. It returns
     //the JSON object to be put into the results Scene.
@@ -102,15 +152,14 @@ public class NobelPrize extends Application {
             System.out.println("Exception" + ex);
         }
         
-        //Create a BufferedReader obj to efficiently hold text from a character stream
+        //Create a Reader obj to hold text from a character stream
         //and use InputStreamReader to decode bytes into characters
-        JSONObject jsonObj = null;
-        Reader reader=null;
-    
+        Reader reader;
         reader = new InputStreamReader(nobelDataStream);
     
         //call readData to put all the text into a string and put in JSONOBject
         String jsonText = readData(reader);
+        JSONObject jsonObj;
         jsonObj = new JSONObject(jsonText);
         
         try {
@@ -150,28 +199,29 @@ public class NobelPrize extends Application {
         String id="";
         String numberOfLaureates="";
         //Search for laureates by country of birth
-        String bornCountry="";;
+        String bornCountry="";
         //Search for laureates by country of birth based on country code
-        String bornCountryCode="";;
+        String bornCountryCode="";
         //Search for laureates by city of birth
-        String bornCity="";;
+        String bornCity="";
         //Search for laureates by country where they died
-        String diedCountry="";;
-        String diedCountryCode="";;
-        String diedCity="";;
-        String bornDate="";;
-        String bornDateTo="";;
-        String diedDate="";;
-        String diedDateTo="";;
+        String diedCountry="";
+        String diedCountryCode="";
+        String diedCity="";
+        String bornDate="";
+        String bornDateTo="";
+        String diedDate="";
+        String diedDateTo="";
         //Search for laureates based on the motivation of receiving the prize
-        String motivation="";;
-        String gender="";;
+        String motivation="";
+        String gender="";
         String affiliation="";
         
         //add in fields to end of URL eg. ?category=phyics&year=2000&yearto=2001';
         //concatenate string eg. url+..+..;  
      
-        //launch JavaFX application to get user input
+       
+        //launch JavaFX application
         launch();
         
     }    
